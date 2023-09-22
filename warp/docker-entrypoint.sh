9e3ace3e93a35ce9704ec7086604e3db08fbb45a
shadowofgost <shadowofgost@outlook.com>
2023-09-22 19:37:03 +0800

    update

--- warp/docker-entrypoint.sh
+++ warp/docker-entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+#!/bin/sh
 # -*- coding: utf-8 -*-
 ###
  # @Author           : Albert Wang
- # @Time             : 2023-08-27 10:33:06
+ # @Time             : 2023-09-22 18:30:01
  # @Description      :
  # @Email            : shadowofgost@outlook.com
- # @FilePath         : /auto-workflow/warp/docker-entrypoint.sh
- # @LastTime         : 2023-08-27 10:33:07
+ # @FilePath         : /okteto-deploy/warp-xray/docker-entrypoint.sh
+ # @LastTime         : 2023-09-22 19:19:14
  # @LastAuthor       : Albert Wang
  # @Software         : Vscode
  #  Copyright Notice : Copyright (c) 2023 Albert Wang 王子睿, All Rights Reserved.
 ###
 
 set -e
-
+/usr/bin/xray -config /etc/xray/config.json &
 warp-svc  &
 sleep 3 &&  echo y | warp-cli --accept-tos register
 warp-cli --accept-tos set-license 1Op9F3g4-8Q9Yn0g6-743O9lrn
