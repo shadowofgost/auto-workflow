b269a3a9142a84f9b74b385d5fe9a138400e806e
Albert Wang <shadowofgost@outlook.com>
2024-02-03 22:04:49 +0800

    Update docker-entrypoint.sh

--- warp/docker-entrypoint.sh
+++ warp/docker-entrypoint.sh
@@ -18,30 +18,20 @@ warp-svc &
 
 # sleep to wait for the daemon to start, default 2 seconds
 sleep "$WARP_SLEEP"
-
-# if /var/lib/cloudflare-warp/reg.json not exists, register the warp client
-if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
-    warp-cli register && echo "Warp client registered!"
-    # if a license key is provided, register the license
-    if [ -n "$WARP_LICENSE_KEY" ]; then
-        echo "License key found, registering license..."
-        warp-cli set-license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
-    fi
-     # Assuming $WARP_LICENSE_KEY contains the value "true" or "false"
-    if [[ "$WARP_PROXY" == "TRUE" ]]; then
-        echo "WARP_PROXY is true."
-        warp-cli --accept-tos set-mode proxy
-    else
-        echo "WARP_PROXY is false."
-    fi
-    # connect to the warp server
-    warp-cli connect
-else
-    echo "Warp client already registered, skip registration"
+warp-cli register && echo "Warp client registered!"
+# if a license key is provided, register the license
+if [ -n "$WARP_LICENSE_KEY" ]; then
+    echo "License key found, registering license..."
+    warp-cli --accept-tos set-license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
 fi
-while true;
-    do
-        warp-cli --accept-tos status
-        sleep 600;
-    done
-exec "$@"
+ # Assuming $WARP_LICENSE_KEY contains the value "true" or "false"
+if [[ "$WARP_PROXY" == "TRUE" ]]; then
+    echo "WARP_PROXY is true."
+    warp-cli --accept-tos set-mode proxy
+else
+    echo "WARP_PROXY is false."
+fi
+# connect to the warp server
+warp-cli --accept-tos connect
+
+warp-cli --accept-tos enable-always-on
